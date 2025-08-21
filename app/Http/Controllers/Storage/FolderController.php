723ea7a7b848c468e1e0bf4082603dbf6edf8457
Christian Holländer <christian@vermessung-hollaender.de>
2025-08-21 16:29:45 +0200

    chore: stuff

--- app/Http/Controllers/Storage/FolderController.php
+++ app/Http/Controllers/Storage/FolderController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Storage;
 
-use App\Data\BreadcrumbData;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\FolderResource;
 use App\Http\Resources\StorageConfigResource;
