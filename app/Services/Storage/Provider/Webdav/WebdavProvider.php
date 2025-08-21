84682bb7f9e23f9ab9154751beb86e3381b3a903
Christian Holländer <christian@vermessung-hollaender.de>
2025-08-21 16:23:59 +0200

    fix: fix config access

--- app/Services/Storage/Provider/Webdav/WebdavProvider.php
+++ app/Services/Storage/Provider/Webdav/WebdavProvider.php
@@ -12,10 +12,10 @@ class WebdavProvider extends LaravelProvider
     {
         return Storage::build([
             'driver' => 'webdav',
-            'baseUri' => $storageConfig->provider_options['base_uri'] ?? '',
-            'userName' => $storageConfig->provider_options['username'] ?? '',
-            'password' => $storageConfig->provider_options['password'] ?? '',
-            'pathPrefix' => $storageConfig->provider_options['path_prefix'] ?? parse_url($storageConfig->provider_options['base_uri'], PHP_URL_PATH) ?? '',
+            'baseUri' => $this->config->provider_options['base_uri'] ?? '',
+            'userName' => $this->config->provider_options['username'] ?? '',
+            'password' => $this->config->provider_options['password'] ?? '',
+            'pathPrefix' => $this->config->provider_options['path_prefix'] ?? parse_url($this->config->provider_options['base_uri'], PHP_URL_PATH) ?? '',
         ]);
     }
 }
