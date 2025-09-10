2b4a21d5d87f81459dcc5db4f57fa48feafd72ba
Christian Holländer <christian@vermessung-hollaender.de>
2025-09-10 14:54:33 +0200

    fix: optimize and fix DashboardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\FileResourceCollection;
-use App\Models\Folder;
+use App\Models\File;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -12,25 +12,21 @@ class DashboardController extends Controller
     public function show(): Response
     {
         $storageConfigs = auth()->user()->storageConfigs;
-        $files = $storageConfigs->flatMap(fn ($config) => $config->allFiles);
+        $baseQuery = File::query()
+            ->whereIn('files.storage_config_id', $storageConfigs->pluck('id'))
+            ->limit(10);
 
         return Inertia::render('Dashboard', [
-            'recentlyEditedFiles' => Inertia::defer(function () use ($files) {
-                return new FileResourceCollection($files->sortByDesc('updated_at')->take(10));
+            'recentlyEditedFiles' => Inertia::defer(function () use ($baseQuery, $storageConfigs) {
+                return new FileResourceCollection(
+                    $baseQuery->clone()->orderByDesc('updated_at')->get()
+                );
             }, 'recentlyEditedFiles'),
-            'recentlyAddedFiles' => Inertia::defer(function () use ($files) {
-                return new FileResourceCollection($files->sortByDesc('created_at')->take(10));
+            'recentlyAddedFiles' => Inertia::defer(function () use ($baseQuery, $storageConfigs) {
+                return new FileResourceCollection(
+                    $baseQuery->clone()->orderByDesc('created_at')->get()
+                );
             }, 'recentlyAddedFiles'),
         ]);
-    }
-
-    protected function files(Folder $folder)
-    {
-        $files = $folder->files;
-        $folder->folders->each(function (Folder $subFolder) use ($files) {
-            $files->push(...$this->files($subFolder));
-        });
-
-        return $files;
     }
 }
