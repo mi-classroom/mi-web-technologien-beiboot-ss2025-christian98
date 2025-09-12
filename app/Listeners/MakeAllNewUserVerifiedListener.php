af845829470be89a7878fdb2e0811598e79b10e5
Christian Holländer <christian@vermessung-hollaender.de>
2025-09-12 12:22:48 +0200

    feat: make all new users verified

---
+++ app/Listeners/MakeAllNewUserVerifiedListener.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+
+/**
+ * Listener to automatically mark all newly registered users as verified.
+ * @todo Remove this listener when email verification is implemented.
+ */
+class MakeAllNewUserVerifiedListener
+{
+    public function __construct()
+    {
+    }
+
+    public function handle(Registered $event): void
+    {
+        $user = $event->user;
+        if ($user instanceof User) {
+            $user->markEmailAsVerified();
+        }
+    }
+}
