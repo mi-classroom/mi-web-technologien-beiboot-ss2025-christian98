3488ef9bc98db63662f5324e5d23091125cb88ff
Christian Holländer <christian@vermessung-hollaender.de>
2025-09-25 11:59:42 +0200

    ci: add missing script

---
+++ scripts/docker-build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+while getopts f:i:t:v:p: flag
+do
+    case "${flag}" in
+        # Dockerfile name
+        f) file=${OPTARG};;
+        i) image=${OPTARG};;
+        t) tag=${OPTARG};;
+        v) version=${OPTARG};;
+        p) push=${OPTARG};;
+        *) exit 1;;
+    esac
+done
+
+docker build \
+  --cache-from "$image":development \
+  -f "$file" \
+  --build-arg APP_VERSION="$version" \
+  -t "$tag" .
+
+
+if [ "$push" == 'true' ]; then
+    echo "Pushing docker image: $image"
+    docker image push "$image" --all-tags
+else
+    echo "Skipping pushing docker image: $image"
+fi
